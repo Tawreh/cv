edee8da6785a3ce4fa01a1e02bb8805886905217
Tawreh <ssk@herbal-jazz.net>
2014-01-29 11:22:16 +0000

    First commit!

---
+++ index.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+  <title>Sophie Shanahan-Kluth</title>
+
+  <meta name="description" content="CV of Bristol-based Web Designer Sophie Shanahan-Kluth">
+  <meta name="viewport" content="width=device-width">
+
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="stylesheet" type="text/css" href="style.css" />
+
+  <script src="http://use.typekit.com/bdi2vhc.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
+</head>
+<body>
+  <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+
+  <div class="circle" id="big-blue"></div>
+  <div class="circle" id="turq"></div>
+  <div class="circle" id="dturq"></div>
+  <div class="circle" id="lturq"></div>
+  <div class="circle" id="lblue"></div>
+
+  <article id="page">
+    <header>
+      <div class="circle" id="dblue"></div>
+      <h1>Sophie Shanahan-Kluth</h1>
+      <h2>
+        <span class="web"><a href="http://herbal-jazz.net">herbal-jazz.net</a></span>
+        <span class="email"><a href="mailto:ssk@herbal-jazz.net">ssk@herbal-jazz.net</a></span>
+        <span class="phone"><a href="tel:+447460776176">07460 776 176</a></span>
+      </h2>
+    </header>
+
+    <section class="about-me">
+      <h2>About me</h2>
+      <p>I’m Sophie, a 23-year-old, 6’2″, female web developer based in Bristol. I moved here from Lincolnshire when I started university and fell head-over-heels for the place. :)</p>
+      <p>A recent graduate of Web Design from UWE in Bristol, I like wearing silly hats, drinking coffee, listening to music, baking, drawing, typography and fantasy writing. And cats. I really like cats.</p>
+    </section>
+
+    <section class="overview">
+      <h2>General overview</h2>
+      <p>I have experience with ...</p>
+      <ul>
+        <li> HTML5 and CSS3, bringing PSD designs to life in the browser</li>
+        <li> PHP and MySQL, mainly through PMA, although some base knowledge with command line SQL</li>
+        <li> Drupal 6/7 with CiviCRM 3.5+, including writing custom modules for each</li>
+        <li> SVN and a little bit of git, limited by the fact I am a Windows user</li>
+        <li> Web accessibility standards and making websites easy-to-use</li>
+        <li> JavaScript and jQuery, used mainly in conjunction with Drupal/Civi</li>
+        <li> Responsive web design, although limited as most of our projects haven’t called for its use</li>
+        <li> WordPress 3.0+, mainly with writing custom themes for personal use</li>
+        <li> Writing project documentation for a range of audiences (clients, other developers)</li>
+      </ul>
+    </section>
+
+    <section class="work">
+      <h2>Work</h2>
+      <dl>
+        <dt>Circle Interactive - July 2011 to present</dt>
+        <dd>
+          <p><a href="http://circle-interactive.co.uk/">Website</a> - <a href="/repo">link to personal repository of work</a></p>
+          <p>I started at Circle for my placement year between July 2011 and September 2012. During my final year of uni (Sept 2012 - May 2013), I worked at Circle while studying, during holidays and once every other week when my timetable allowed. I was hired straight out of university, starting full time as soon as my exams finished at the end of May 2013.</p>
+          <p>My work included:</p>
+          <ul>
+            <li> Creating themes for Drupal 6 and 7 sites, both from provided PSDs and based on client suggestions of “liked sites”</li>
+            <li> Working directly with clients through our support system and also through direct contact on email and telephone (rarely!)</li>
+            <li> Creating custom modules - see personal repository for the best examples, but these modules include: a rewritten support system for our intranet; dynamic data retrieval from a client’s server to show course schedules; working with geolocation on an Apache server; updating a cookie control module to work with Drupal 5, 6 and 7 for distribution to our clients; and updating a half-written module to perform a proximity search on CiviCRM data</li>
+            <li> Writing documentation for clients and colleagues on how to use existing Drupal modules/CiviCRM extensions, custom modules created for a purpose, and documentation for theming</li>
+            <li> Using existing modules to create complex functionality on sites, particularly using views, content access and internationalisation/translation</li>
+            <li> Estimating work needed and time required for projects, and advising on what technologies could be used to solve problems</li>
+          </ul>
+        </dd>
+
+        <dt>Bristol Women’s Voice - September 2012 to May 2013</dt>
+        <dd>
+          <p><a href="http://bristolwomensvoice.org.uk/">Website</a> - <a href="/cv/bwv/report.pdf">final report</a> - <a href="/cv/bwv/docs.pdf">associated documentation</a></p>
+          <p>As part of a final-year module I worked closely with Louise Williams at BWV to update some of the styling and layout on their website.</p>
+          <p>As a team, we also created some user documentation to allow other members of her organisation to update content on the site. This involved teaching my team mates how WordPress works so they could produce some basic documentation as well.</p>
+          <p>The document here is our final report, which should give an idea of what we did and how. It’s a bit wordy, but it was a business module, and we were expected to write between 30-40 pages!</p>
+        </dd>
+
+        <dt>Moto Guzzi Club - intermittently since July 2010</dt>
+        <dd>
+          <p><a href="http://motoguzziclub.co.uk/">Website</a> - <a href="http://motoguzziclub.co.uk/regalia/">link to work</a></p>
+          <p><strong>July - Sept 2010</strong>: Redesigned the regalia section and added Paypal links for all items, including the membership section. This included learning ASP, and streamlining some of the site - mainly the header, which I separated out into its own file to make it easier to update links in the future.</p>
+          <p><strong>Sept 2010 - present</strong>: Intermittent support, including: updating stylesheets, changing prices in Paypal, teaching the membership secretary how to update parts of the site herself.</p>
+          <p><strong>Nov 2013 - present</strong>: Some consultancy on finding a new webmaster and host.</p>
+        </dd>
+      </dl>
+    </section>
+
+    <section class="study">
+      <h2>Study</h2>
+      <dl>
+        <dt>BSc(hons) in Web Design</dt>
+        <dd>
+          <p>Studied at <abbr title="University of the West of England">UWE</abbr> Bristol from September 2009 to July 2013, graduating with honours.</p>
+          <p>Modules included:</p>
+          <ul>
+            <li> Final year: a digital media project (<a href="http://herbal-jazz.net/fontastica/beta/index.php">Fontastica, a font finder</a>), interaction design (<a href="http://id.herbal-jazz.net/">logbook of work</a>), consultancy project (working with Bristol Women’s Voice), advanced topics in web development (creating a RESTful API system)</li>
+            <li> Third year: placement at <a href="http://circle-interactive.co.uk/">Circle Interactive</a></li>
+            <li> Second year: human-computer interaction, information architecture, web design principles</li>
+            <li> First year: information design, media technologies (basic knowledge of Flash and ActionScript 3.0 programming)</li>
+          </ul>
+        </dd>
+
+        <dt>A-levels</dt>
+        <dd>
+          <p>Attended <abbr title="Sleaford Joint Sixth Form">SJSF</abbr> in Lincolnshire from September 2007 to July 2009. Left with three A-levels and one BTEC Certificate in Art &amp; Design (grade MM).</p>
+          <p>BTEC units included: photography, fashion illustration, textile work, art history, logo/brand design, web design (in final major project, self-created brief), typography.</p>
+          <p>A-levels achieved in English Language, Chemistry and General Studies.</p>
+        </dd>
+      </dl>
+    </section>
+
+    <section class="what">
+      <h2>What I can bring to the table</h2>
+      <p>I’ve been working with the web since I was 14, and since then I’ve become a huge believer in making things simple and easy to use, both for the end user and for any developer who will take up work after me. I’m keen to broaden my horizons and learn new things, rather than stagnating in a single place.</p>
+      <p>During my placement and subsequent employment I feel I’ve become a stronger team player, and I work well with like-minded and enthusiastic people. However, I thrive best when given a task to do and a deadline, and am capable of putting my head down and concentrating on a single project.</p>
+      <p>I enjoy learning and sharing new knowledge with others. I keep up with news on Twitter, listen to web-related podcasts, and, when possible, I attend conferences. As a placement student I learned from my colleagues, and try to pass on that help to students in turn, whenever possible. Occasionally I <a href="http://herbal-jazz.net/category/work/drupal/">blog about the work I do</a>, and these posts have proven to be some of the most popular I’ve written.</p>
+      <p>Oh ... and I really like writing documentation! I know that sounds weird, but I think it’s important to make it as easy as possible for others to pick up systems. I get frustrated when documentation isn’t provided, so I do my best to provide it for others. I have some of my documentation samples <a href="https://drive.google.com/folderview?id=0B8GSoyQxjjZ1TTRpMF91UlNtUDg&usp=sharing">saved on Google Drive</a>.</p>
+    </section>
+
+    <section class="references">
+      <h2>References</h2>
+      <dl>
+        <dt>David Moreton</dt>
+        <dd>
+          <p>My boss at Circle Interactive<br />
+            <a href="mailto:davem@circle-interactive.co.uk">davem@circle-interactive.co.uk</a> · <a href="http://circle-interactive.co.uk/">website</a> · <a href="tel:+441179096967">0117 909 6967</a></p>
+        </dd>
+
+        <dt>Andrew Walker</dt>
+        <dd>
+          <p>Senior developer at Circle Interactive<br />
+            <a href="mailto:andyw@circle-interactive.co.uk">andyw@circle-interactive.co.uk</a></p>
+        </dd>
+
+        <dt>Praminda Caleb-Solly</dt>
+        <dd>
+          <p>Senior lecturer at UWE<br />
+            <a href="mailto:pcalebso@uwe.ac.uk">pcalebso@uwe.ac.uk</a> · <a href="http://www.cems.uwe.ac.uk/~pcalebso/">website</a> · <a href="tel:+441173283178">0117 328 3178</a></p>
+      </dl>
+    </section>
+
+  </article>
+
+</body>
+</html>
